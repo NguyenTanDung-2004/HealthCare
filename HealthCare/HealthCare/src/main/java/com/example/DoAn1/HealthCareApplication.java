59ee43def651449f9bba848c487ad4214c7363b5
Nguyen Tan Dung <22520001@gm.uit.edu.vn>
2025-01-03 21:23:08 +0700

    complete

--- HealthCare/HealthCare/src/main/java/com/example/DoAn1/HealthCareApplication.java
+++ HealthCare/HealthCare/src/main/java/com/example/DoAn1/HealthCareApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DoAn1Application {
+public class HealthCareApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DoAn1Application.class, args);
+		SpringApplication.run(HealthCareApplication.class, args);
 	}
 
 }
